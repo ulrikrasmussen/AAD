3fcda614fac28963c14e945a76b74d6defe6741d
Jens Raaby (laursen laptop) <jens+github@m.raaby.co.uk>
2012-05-11 11:43:28 +0200

    added min cost problem but need to check that sharing the variables between the problems does not affect the outcome

--- assignment1/flow.py
+++ assignment1/flow.py
@@ -1,15 +1,11 @@
 import json,sys
 from pulp import *
 
-print_debug = True
-simple = False
+print_debug = False
 
 
 # Import the graph json file
-if simple:
-	filename = "simple.json"
-else:
-	filename = "network.json"
+filename = "network.json"
 f = open(filename, "r")
 edges = json.load(f)["edges"]
 f.close()
@@ -66,18 +62,18 @@ for edge in edges:
 	network.connect(v,u,LpVariable(str(v) + "_to_" + str(u),lower,upper))
 	# Store a cost constraint
 	if u in external_relays or v in external_relays:
-		cost_network.connect(u,v,LpVariable("Expensive_" + str(u) + "_to_" + str(v),1,1))
-		cost_network.connect(v,u,LpVariable("Expensive_" + str(v) + "_to_" + str(u),1,1))
+		cost_network.connect(u,v,1)
+		cost_network.connect(v,u,1)
 	else: # All internal connections must be free (zero cost)
-		cost_network.connect(u,v,LpVariable("Free_" + str(u) + "_to_" + str(v),0,0))
-		cost_network.connect(v,u,LpVariable("Free_" + str(v) + "_to_" + str(u),0,0))
+		cost_network.connect(u,v,0)
+		cost_network.connect(v,u,0)
 
 # add a super source to workstations
-if not simple:
-	vertices.add("source")
-	for src in workstations:
-		# note no upper bound is specified!
-		network.connect("source",src,LpVariable("source_to_"+str(src),0,None))
+vertices.add("source")
+for src in workstations:
+	# note no upper bound is specified!
+	network.connect("source",src,LpVariable("source_to_"+str(src),0,None))
+	cost_network.connect("source",src,0)
 
 # Check it's all in the data structure
 if (print_debug):
@@ -86,21 +82,27 @@ if (print_debug):
 	print vertices
 
 # Begin solving using Pulp
-prob = LpProblem("Max flow network transfer problem",LpMaximize)
-prob_cost = LpProblem("Minimum cost flow network transfer problem",LpMinimize)
-variables = network.get_dict()
-cost_variables = cost_network.get_dict()
+prob 		= LpProblem("Max flow network transfer problem",LpMaximize)
+prob_cost 	= LpProblem("Minimum cost flow network transfer problem",LpMinimize)
+variables 	= network.get_dict()
+costs 		= cost_network.get_dict()
+dupvariables= network.get_dict()
 
 
-# Collect all the flow out of the source - note nothing flows in
-source_connected = []
+# Collect variables for the objective functions
+source_connected 	= [] # maximise the flow from the supersource
+cost_minimisation 	= [] # minimise the network transfer costs
+
 for var in variables:
+	# Add the source-connected edges for flow maximisation
 	if var[0] == "source":
 		source_connected.append(variables[var])
+	# Collect the cost minimisation term for every edge:
+	cost_minimisation.append(costs[var]*variables[var])
 
 # Add the objective functions
 prob 		+= lpSum(source_connected), "Flow out of super source"
-prob_cost	+= lpSum(), "Minimise the overall cost"
+prob_cost	+= lpSum(cost_minimisation), "Network transfer costs"
 
 # Add constraints on flow conservation
 for vertex in vertices:
@@ -114,15 +116,34 @@ for vertex in vertices:
 			flow_in.append(variables[var])
 		elif var[0] == vertex:
 			flow_out.append(variables[var])
-	prob += lpSum(flow_in) - lpSum(flow_out) == 0, "Flow conservation for " + str(vertex)
+	flow_conservation = lpSum(flow_in) - lpSum(flow_out) == 0, "Flow conservation for " + str(vertex)
+	prob 		+= flow_conservation
+	prob_cost	+= flow_conservation
 
+# Solve the max flow problem
 prob.writeLP("maxflow.lp")
 prob.solve()
 
 print "Max flow Status: ", LpStatus[prob.status]
 print "Max flow Value: ", value(prob.objective)
 
-# Print out the actual flows:
+
+# add the optimal max flow as a constraint on the minimal cost problem:
+prob_cost += lpSum(source_connected) == value(prob.objective), "Flow must be the maximum"
+prob_cost.writeLP("mincost.lp")
+prob_cost.solve()
+
+
+print "Min cost Status: ", LpStatus[prob_cost.status]
+print "Min cost Value: ", value(prob_cost.objective)
+
+print "The following costs were incurreed by using external connections in the optimal solution"
+for cost in costs:
+	if not costs[cost] == 0: # only include the costly edges
+		optimal_cost = value(costs[cost])*value(variables[cost])
+		print cost,"optimal cost: ",value(costs[cost])*value(variables[cost])
+
+# Print out the optimal flows:
 if print_debug:
 	for var in variables:
-		print value(variables[var])
+		print str(var),":", value(variables[var])
